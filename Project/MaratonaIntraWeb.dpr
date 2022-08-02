0c4fed3605c4411615ef4f912e15daab9546097f
Fernando Luis Loff <fernandoloff123@gmail.com>
2022-08-02 11:44:02 -0300

    Primeiro commit da Maratona Intraweb

---
+++ Project/MaratonaIntraWeb.dpr
@@ -0,0 +1,19 @@
+program MaratonaIntraWeb;
+
+uses
+  IWRtlFix,
+  IWJclStackTrace,
+  IWJclDebug,
+  Forms,
+  IWStart,
+  uFrmPadrao in '..\Source\Padrao\uFrmPadrao.pas' {FrmPadrao: TIWAppForm},
+  uServerController in '..\Source\Controller\uServerController.pas' {IWServerController: TIWServerControllerBase},
+  uUserSessionUnit in '..\Source\Controller\uUserSessionUnit.pas' {IWUserSession: TIWUserSessionBase},
+  uFrmLogin in '..\Source\Page\uFrmLogin.pas' {FrmLogin: TIWAppForm},
+  Template.StarAdmin in '..\Library\Template.StarAdmin.pas';
+
+{$R *.res}
+
+begin
+  TIWStart.Execute(True);
+end.
