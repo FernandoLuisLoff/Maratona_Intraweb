946649050cd9d90e12d6bd9d6e5bcb6170a62d88
Fernando Luis Loff <fernandoloff123@gmail.com>
2022-08-11 08:06:19 -0300

    Aula 10 - Implementando Callback

--- Library/Template.StarAdmin.pas
+++ Library/Template.StarAdmin.pas
@@ -15,13 +15,13 @@ implementation
 Function Get_Head : String;
 begin
 
-    Result := ' <meta charset="utf-8">                                                                             '+
-              ' <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">             '+
+    Result := ' <meta charset="utf-8">                                                                                 '+
+              ' <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">                 '+
               ' <link rel="stylesheet" href="' + PathTheme + 'vendors/iconfonts/mdi/css/materialdesignicons.min.css">  '+
               ' <link rel="stylesheet" href="' + PathTheme + 'vendors/iconfonts/ionicons/dist/css/ionicons.css">       '+
               ' <link rel="stylesheet" href="' + PathTheme + 'vendors/iconfonts/flag-icon-css/css/flag-icon.min.css">  '+
-//              ' <link rel="stylesheet" href="' + PathTheme + 'vendors/css/vendor.bundle.base.css">                     '+
-//              ' <link rel="stylesheet" href="' + PathTheme + 'vendors/css/vendor.bundle.addons.css">                   '+
+              ' <link rel="stylesheet" href="' + PathTheme + 'vendors/css/vendor.bundle.base.css">                     '+
+              ' <link rel="stylesheet" href="' + PathTheme + 'vendors/css/vendor.bundle.addons.css">                   '+
               ' <link rel="stylesheet" href="' + PathTheme + 'css/shared/style.css">                                   '+
               ' <link rel="stylesheet" href="' + PathTheme + 'css/demo_1/style.css">                                   '+
               ' <link rel="shortcut icon" href="' + PathTheme + 'images/favicon.ico" />                                ';
@@ -34,8 +34,8 @@ begin
     Result := ' <script src="' + PathTheme + 'vendors/js/vendor.bundle.base.js"></script>                   '+
               ' <script src="' + PathTheme + 'vendors/js/vendor.bundle.addons.js"></script>                 '+
               ' <script src="' + PathTheme + 'js/shared/off-canvas.js"></script>                            '+
-              ' <script src="' + PathTheme + 'js/shared/misc.js"></script>                                  ';
-//              ' <script src="' + PathTheme + 'js/shared/jquery.cookie.js" type="text/javascript"></script>  ';
+              ' <script src="' + PathTheme + 'js/shared/misc.js"></script>                                  '+
+              ' <script src="' + PathTheme + 'js/shared/jquery.cookie.js" type="text/javascript"></script>  ';
 
 end;
 
@@ -48,7 +48,7 @@ begin
               ' <link rel="stylesheet" href="' + PathTheme + 'vendors/iconfonts/ionicons/dist/css/ionicons.css">        '+
               ' <link rel="stylesheet" href="' + PathTheme + 'vendors/iconfonts/flag-icon-css/css/flag-icon.min.css">   '+
               ' <link rel="stylesheet" href="' + PathTheme + 'vendors/css/vendor.bundle.base.css">                      '+
-//              ' <link rel="stylesheet" href="' + PathTheme + 'vendors/css/vendor.bundle.addons.css">                    '+
+              ' <link rel="stylesheet" href="' + PathTheme + 'vendors/css/vendor.bundle.addons.css">                    '+
               ' <link rel="stylesheet" href="' + PathTheme + 'css/shared/style.css">                                    '+
               ' <link rel="shortcut icon" href="' + PathTheme + 'images/favicon.ico" />                                 ';
 
@@ -58,10 +58,10 @@ Function Get_Footer_Login : String;
 begin
 
     Result := ' <script src="' + PathTheme + 'vendors/js/vendor.bundle.base.js"></script>                  '+
-//              ' <script src="' + PathTheme + 'vendors/js/vendor.bundle.addons.js"></script>                '+
-              ' <script src="' + PathTheme + 'js/shared/off-canvas.js"></script>                           ';
-//              ' <script src="' + PathTheme + 'js/shared/misc.js"></script>                                 '+
-//              ' <script src="' + PathTheme + 'js/shared/jquery.cookie.js" type="text/javascript"></script> ';
+              ' <script src="' + PathTheme + 'vendors/js/vendor.bundle.addons.js"></script>                '+
+              ' <script src="' + PathTheme + 'js/shared/off-canvas.js"></script>                           '+
+              ' <script src="' + PathTheme + 'js/shared/misc.js"></script>                                 '+
+              ' <script src="' + PathTheme + 'js/shared/jquery.cookie.js" type="text/javascript"></script> ';
 
 end;
 
--- Source/Page/uFrmIndex.pas
+++ Source/Page/uFrmIndex.pas
@@ -10,9 +10,11 @@ Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classe
 
 type
   TFrmIndex = class(TFrmPadrao)
+    procedure IWAppFormCreate(Sender: TObject);
   private
     { Private declarations }
   public
+    Procedure prc_Acoes(EventParams : TStringList);
     { Public declarations }
   end;
 
@@ -22,5 +24,56 @@ var
 implementation
 
 {$R *.dfm}
+
+uses uFrmLogin;
+
+procedure TFrmIndex.IWAppFormCreate(Sender: TObject);
+begin
+  inherited;
+  WebApplication.RegisterCallBack('prc_Acoes',prc_Acoes);
+end;
+
+procedure TFrmIndex.prc_Acoes(EventParams : TStringList);
+var
+  sl: TStringList;
+  IdAcao,i,CountParam: Integer;
+  Acao,s: String;
+
+begin
+
+  sl := TStringList.Create;
+  CountParam := 0;
+
+  try
+
+    sl.StrictDelimiter := True;
+    sl.CommaText := EventParams.Values['Params'];
+    s := sl.CommaText;
+
+    CountParam := TStringList(sl).Count;
+
+    for I := 0 to pred(CountParam) do begin
+
+      if I = 0 then
+        Acao := sl.ValueFromIndex[I];
+
+      if I = 1 then
+        IdAcao := StrToInt(sl.ValueFromIndex[I]);
+
+    end;
+
+    if Acao = 'Fechar' then begin
+
+      Self.Release;
+      FrmLogin := TFrmLogin.Create(nil);
+      FrmLogin.show;
+
+    end;
+
+  except
+
+  end;
+
+end;
 
 end.
